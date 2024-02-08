b1284e7810127dbdfbf18dadcff2f75996bcb8f2
Калинин Валентин Сергеевич <valentin.kalinin@ligastavok.ru>
2024-02-08 23:17:43 +0300

    Secure attached activities

--- android/src/main/kotlin/com/flutterplaza/no_screenshot/NoScreenshotPlugin.kt
+++ android/src/main/kotlin/com/flutterplaza/no_screenshot/NoScreenshotPlugin.kt
@@ -1,6 +1,9 @@
 package com.flutterplaza.no_screenshot
 
 import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+
 import android.content.Context
 import androidx.annotation.NonNull
 
@@ -15,65 +18,99 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 
 
 /** NoScreenshotPlugin */
-class NoScreenshotPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-  private lateinit var context: Context
-  private lateinit var activity: Activity
+class NoScreenshotPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private lateinit var context: Context
+    private lateinit var activity: Activity
+    private var flags: Int = 0
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "com.flutterplaza.no_screenshot")
-    channel.setMethodCallHandler(this)
-    context = flutterPluginBinding.applicationContext
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel =
+            MethodChannel(flutterPluginBinding.binaryMessenger, "com.flutterplaza.no_screenshot")
+        channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
 
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) =
-    if (call.method == "screenshotOff") {
-        screenshotOff();
-      result.success(true);
-    }
-    else if(call.method == "screenshotOn"){
-     screenshotOn();
-      result.success(true);
-    }
-    else if(call.method == "toggleScreenshot"){
-      var flags: Int = activity.window.attributes.flags;
-     if( (flags and LayoutParams.FLAG_SECURE) != 0){
-       screenshotOn();
-      }else {
-        screenshotOff();
-     }
-      result.success(true);
-    }
-    else {
-      result.notImplemented()
+        val app: Application? = context as? Application
+        if (app != null) {
+            // Successfully casted to Application
+            app.registerActivityLifecycleCallbacks(SecureActivityLifecycleCallbacks())
+        }
     }
 
-  private fun screenshotOff(){
-    activity.window.addFlags(LayoutParams.FLAG_SECURE);
-  }
-  private  fun screenshotOn(){
-    activity.window.clearFlags(LayoutParams.FLAG_SECURE);
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) =
+        if (call.method == "screenshotOff") {
+            screenshotOff();
+            result.success(true);
+        } else if (call.method == "screenshotOn") {
+            screenshotOn();
+            result.success(true);
+        } else if (call.method == "toggleScreenshot") {
+            flags = activity.window.attributes.flags;
+            if ((flags and LayoutParams.FLAG_SECURE) != 0) {
+                screenshotOn();
+            } else {
+                screenshotOff();
+            }
+            result.success(true);
+        } else {
+            result.notImplemented()
+        }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    private fun screenshotOff() {
+        flags = LayoutParams.FLAG_SECURE;
+        activity.window.addFlags(flags);
+    }
 
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    activity = binding.activity;
-  }
+    private fun screenshotOn() {
+        activity.window.clearFlags(flags);
+        flags = 0
+    }
 
-  override fun onDetachedFromActivityForConfigChanges() {}
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    activity = binding.activity;
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity;
+    }
 
-  }
+    override fun onDetachedFromActivityForConfigChanges() {}
 
-  override fun onDetachedFromActivity() {}
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity;
+    }
+
+    override fun onDetachedFromActivity() {}
+
+    inner class SecureActivityLifecycleCallbacks : Application.ActivityLifecycleCallbacks {
+
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            if ((flags and LayoutParams.FLAG_SECURE) != 0) {
+                activity.window.addFlags(LayoutParams.FLAG_SECURE)
+            }
+        }
+
+        override fun onActivityStarted(activity: Activity) {
+        }
+
+        override fun onActivityResumed(activity: Activity) {
+        }
+
+        override fun onActivityPaused(activity: Activity) {
+        }
+
+        override fun onActivityStopped(activity: Activity) {
+        }
+
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+        }
+
+        override fun onActivityDestroyed(activity: Activity) {
+        }
+    }
 }
+   
